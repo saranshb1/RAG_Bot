c5a89fb00cfb245c4ca1e57b86827e394ab316fc
saranshbh <saransh19bhargava@gmail.com>
2024-09-15 20:09:54 +0100

    Remove sensitive information

---
+++ __init__.py
@@ -0,0 +1,7 @@
+
+'''
+This project is taken from the langchain library in order to create
+a conversational bot after reading through PDF files.
+
+The project uses open source LLM models to run a question and answer bot.
+'''
